2eee08ad4b794d710701fcd43d1103b0d4a2947e
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-12 14:12:17 +0100

    Create NavbarData.jsx

---
+++ src/Components/Navbar/NavbarData.jsx
@@ -0,0 +1,34 @@
+import DribbleIcon from "../../assets/6000113b522b1655c42d9a2f_dribbble-white.svg"
+import VimeoIcon from "../../assets/6000113b0709454067001834_vimeo-white.svg"
+import BehanceIcon from "../../assets/6000113bd9f0ea6ec6a70b89_behance-white.svg"
+
+export const NavbarData = [
+  {
+    image: DribbleIcon,
+    path: "",
+    className: "",
+  },
+
+  {
+    image: VimeoIcon,
+    path: "",
+    className: "",
+  },
+
+  {
+    image: BehanceIcon,
+    path: "",
+    className: "",
+  }
+
+  // {
+  //   title: "Login",
+  //   path: "/login",
+  //   className: "laptop:hidden flex",
+  // },
+  // {
+  //   title: "Sign Up",
+  //   path: "/signup",
+  //   className: "laptop:hidden flex",
+  // }
+];
