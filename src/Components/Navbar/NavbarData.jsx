4595a80ccfcb81fe52ef3989285fd95460a49553
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-12 14:26:17 +0100

    Update NavbarData.jsx

--- src/Components/Navbar/NavbarData.jsx
+++ src/Components/Navbar/NavbarData.jsx
@@ -2,30 +2,20 @@
 export const NavbarData = [
   {
     title: 'Home',
-    path: "",
+    path: "/",
     className: "",
   },
 
   {
-    title: 'About',
-    path: "",
+    title: 'About Us',
+    path: "/",
     className: "",
   },
 
   {
     title: 'Contact',
-    path: "",
+    path: "/",
     className: "",
   }
 
-  // {
-  //   title: "Login",
-  //   path: "/login",
-  //   className: "laptop:hidden flex",
-  // },
-  // {
-  //   title: "Sign Up",
-  //   path: "/signup",
-  //   className: "laptop:hidden flex",
-  // }
 ];
