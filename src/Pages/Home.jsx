d6869019ac554c6ff22b95d893fc4a2b281608e3
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-12 14:21:10 +0100

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,29 +1,20 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import Palm from "../assets/palm-frond.jpeg";
-import ChainFence from "../assets/chain-fence.jpg";
-import IceSkating from "../assets/ice-skating.jpg";
-import Shrug from "../assets/shrug.jpeg";
-import Eye from "../assets/eye.jpg";
-import Freckles from "../assets/freckles.jpg";
-import Boat from "../assets/boat.jpg";
-import RugLady from "../assets/rug-lady.jpg";
-import PillowLady from "../assets/pillow-lady.jpg";
-import Thinking from "../assets/thinking.jpg";
-import Video1 from "../assets/agency-video.mp4";
+// import Thinking from "../assets/thinking.jpg";
+// import Video1 from "../assets/agency-video.mp4";
 
-const imageStyles = [
-  "w-full laptop:w-1/2",         // Style for the first image
-  "w-full laptop:w-1/2",         // Style for the second image
-  "w-full laptop:w-1/2",         // Style for the third image
-  "w-full laptop:w-1/2",         // Style for the fourth image
-  "w-full laptop:w-1/2",         // Style for the fifth image
-  "w-full laptop:w-1/2",         // Style for the sixth image
-  "w-full laptop:w-1/2",         // Style for the seventh image
-  "w-full laptop:w-1/2",         // Style for the eighth image
-  "w-full laptop:w-1/2",         // Style for the ninth image
-  "w-full laptop:w-1/2",         // Style for the tenth image
-];
+// const imageStyles = [
+//   "w-full laptop:w-1/2",         // Style for the first image
+//   "w-full laptop:w-1/2",         // Style for the second image
+//   "w-full laptop:w-1/2",         // Style for the third image
+//   "w-full laptop:w-1/2",         // Style for the fourth image
+//   "w-full laptop:w-1/2",         // Style for the fifth image
+//   "w-full laptop:w-1/2",         // Style for the sixth image
+//   "w-full laptop:w-1/2",         // Style for the seventh image
+//   "w-full laptop:w-1/2",         // Style for the eighth image
+//   "w-full laptop:w-1/2",         // Style for the ninth image
+//   "w-full laptop:w-1/2",         // Style for the tenth image
+// ];
 
 const animationConfiguration = {
   initial: { opacity: 0 },
@@ -53,81 +44,6 @@ const Home = () => {
 
         <div className="my-36 hidden laptop:block laptop:my-0 mx-4 laptop:mx-0">
           <div className="laptop:mx-[7rem] mb-16">
-            {[Palm, ChainFence, IceSkating, Shrug, Eye, Freckles, Boat, RugLady, PillowLady, Thinking].map((image, index) => (
-              <div key={index} className={`relative ${imageStyles[index]} mx-auto my-2`}>
-                <div
-                  className="relative group"
-                  onMouseEnter={() => setHoveredVideo(index)}
-                  onMouseLeave={() => setHoveredVideo(null)}
-                >
-                  <img
-                    src={image}
-                    className="laptop:w-[] w-full my-2 cursor-pointer"
-                    alt=""
-                  />
-                  {hoveredVideo === index && (
-                    <div className="absolute hidden inset-0 laptop:flex items-center justify-center group-hover:opacity-100 opacity-0 transition-opacity duration-500">
-                      <video
-                        src={Video1}
-                        className="w-full h-full object-cover"
-                        autoPlay
-                        loop
-                        muted
-                        preload="auto"
-                      />
-                      <div className="text-container text-center absolute text-white">
-                        <motion.h1
-                          initial={{ opacity: 0, y: 20 }}
-                          animate={{ opacity: 1, y: 0 }}
-                          transition={{ duration: 0.5 }}
-                          className="text-[1.5rem] font-bold"
-                        >
-                          Common Ground
-                        </motion.h1>
-                        <motion.p
-                          initial={{ opacity: 0, y: 20 }}
-                          animate={{ opacity: 1, y: 0 }}
-                          transition={{ duration: 0.5 }}
-                        >
-                          Partner up to reinvent pop <br />
-                          culture and street style
-                        </motion.p>
-                      </div>
-
-                    </div>
-                  )}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        {/* mobile */}
-        <div className="my-36 laptop:hidden laptop:my-0 mx-4 laptop:mx-0">
-          <div className="laptop:mx-[7rem] mx-4">
-
-            {[Palm, ChainFence, IceSkating, Shrug, Eye, Freckles, Boat, RugLady, PillowLady, Thinking].map((image, index) => (
-              <div key={index} className={`relative ${imageStyles[index]} mx-auto my-4`}>
-                <img
-                  src={image}
-                  className="laptop:w-[] w-full cursor-pointer"
-                  alt=""
-                />
-
-                <div className="absolut w-full items-center justify-center">
-                  <div className="text-center w-auto py-12 text-white bg-black">
-                    <h1 className="text font-black text-[1.4rem]">
-                      Common Ground
-                    </h1>
-                    <p className="">
-                      Partner up to reinvent pop <br />
-                      culture and street style
-                    </p>
-                  </div>
-                </div>
-
-              </div>
-            ))}
 
           </div>
 
