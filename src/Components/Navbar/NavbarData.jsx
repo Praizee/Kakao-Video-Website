95807d6ee4729ea6e2eb076c16c5632ad535f85a
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-13 10:31:13 +0100

    Update NavbarData.jsx

--- src/Components/Navbar/NavbarData.jsx
+++ src/Components/Navbar/NavbarData.jsx
@@ -9,25 +9,25 @@ export const NavbarData = [
   {
     title: 'Company',
     path: "/",
-    className: "text-[0.8rem]",
+    className: "text-[0.8rem] hover:text-white/50 transition duration-500 ease-linear",
   },
 
   {
     title: 'Works',
     path: "/",
-    className: "text-[0.8rem]",
+    className: "text-[0.8rem] hover:text-white/50 transition duration-500 ease-linear",
   },
 
   {
     title: 'Blog',
     path: "/",
-    className: "text-[0.8rem]",
+    className: "text-[0.8rem] hover:text-white/50 transition duration-500 ease-linear",
   },
 
   {
     title: 'All Pages',
     path: "/",
-    className: "text-[0.8rem]",
+    className: "text-[0.8rem] hover:text-white/50 transition duration-500 ease-linear",
   }
 
 ];
