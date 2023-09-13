7ddef09e41285f8ede7617d3d5ff0d74d220f4d1
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-13 12:07:27 +0100

    Update NavbarData.jsx

--- src/Components/Navbar/NavbarData.jsx
+++ src/Components/Navbar/NavbarData.jsx
@@ -5,29 +5,34 @@ export const NavbarData = [
     // path: "/", 
     className: "text-[0.8rem] hover:text-white/50 cursor-pointer transition duration-500 ease-linear",
     content: ['Demo 1', 'Demo 2', 'Demo 3'],
+    dropdownclass: 'block text-white hover:text-white text-white/50 hover:translate-x-4 transition duration-300',
   },
   {
     title: 'Company',
     // path: "/",
     className: "text-[0.8rem] hover:text-white/50 cursor-pointer transition duration-500 ease-linear",
     content: ['About Us', 'Our Team', 'Mission & Vision'],
+    dropdownclass: 'block text-white/50 hover:text-white hover:translate-x-4 transition duration-300',
   },
   {
     title: 'Works',
     // path: "/",
     className: "text-[0.8rem] hover:text-white/50 cursor-pointer transition duration-500 ease-linear",
     content: ['Work 1', 'Work 2', 'Work 3'],
+    dropdownclass: 'block text-white/50 hover:text-white hover:translate-x-4 transition duration-300',
   },
   {
     title: 'Blog',
     // path: "/",
     className: "text-[0.8rem] hover:text-white/50 cursor-pointer transition duration-500 ease-linear",
-    content: ['Blog Post 1', 'Blog Post 2', 'Blog Post 3'],
+    content: [], // No content for this one
+    dropdownclass: 'block text-white/50 hover:text-white hover:translate-x-4 transition duration-300',
   },
   {
     title: 'All Pages',
     // path: "/",
     className: "text-[0.8rem] hover:text-white/50 cursor-pointer transition duration-500 ease-linear",
-    content: [], // No content for this one
+    content: [],
+    dropdownclass: 'block text-white/50 hover:text-white hover:translate-x-4 transition duration-300',
   },
 ];
