9b8d3c5bb023f99bb2ee31c7131e61150c6a3471
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-14 14:20:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from "./Pages/Layout";
 import HomePage from "./Pages/Home";
-import AboutUs from "./Pages/AboutUs";
-import Contact from "./Pages/Contact";
-import LoginPage from "./Pages/Login";
-import SignUpPage from "./Pages/SignUp";
-import Details from "./Pages/Details";
+// import AboutUs from "./Pages/AboutUs";
+// import Contact from "./Pages/Contact";
+// import LoginPage from "./Pages/Login";
+// import SignUpPage from "./Pages/SignUp";
+import Blog from "./Pages/Blog";
 import NotFoundPage from "./Pages/404 Page/NotFoundPage";
 
 export default function App() {
@@ -16,11 +16,11 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="about-us" element={<AboutUs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="details" element={<Details />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="signup" element={<SignUpPage />} />
+          {/* <Route path="about-us" element={<AboutUs />} />
+          <Route path="contact" element={<Contact />} /> */}
+          <Route path="blog" element={<Blog />} />
+          {/* <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} /> */}
           {/* This is the catch-all route */}
           <Route path="*" element={<NotFoundPage />} />
         </Route>
