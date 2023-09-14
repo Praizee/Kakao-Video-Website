d319a8bdf05c66e9827ecbf4c07f1956f3930a87
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-14 15:16:37 +0100

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -13,8 +13,8 @@ const Footer = () => {
         <div className="laptop:-mr-[9rem] laptop:-ml-10">
 
           <div className="tablet:hidde laptop:min-w-[26rem] tablet:min-w-[19rem] laptop:block">
-            <span className="text-[0.85rem] text-white font-medium laptop:mb-4">
-              <p className="">
+            <span className="text-white font-medium laptop:mb-4">
+              <p className="text-[0.9rem]">
                 /VIDEO CREATORS
               </p>
             </span>
@@ -25,26 +25,26 @@ const Footer = () => {
 
         <div className="footer uppercase text-white/50">
           <div className="laptop:ml-[rem] mb-8 laptop:mb-0 tablet:mb-0">
-            <span className="text-[0.85rem] text-white font-medium laptop:mb-4">/NAVIGATION</span>
-            <a className="link link-hover font-normal leading-[1.6rem]">Home v.1</a>
-            <a className="link link-hover font-normal leading-[1.6rem]">Home v.2</a>
-            <a className="link link-hover font-normal leading-[1.6rem]">Home v.3</a>
-            <a className="link link-hover font-normal leading-[1.6rem]">Contact</a>
-            <a className="link link-hover font-normal leading-[1.6rem]">Privacy Policy</a>
+            <span className="text-[0.9rem] text-white font-medium laptop:mb-4">/NAVIGATION</span>
+            <a className="link link-hover font-normal text-[0.875rem] leading-[1.6rem]">Home v.1</a>
+            <a className="link link-hover font-normal text-[0.875rem] leading-[1.6rem]">Home v.2</a>
+            <a className="link link-hover font-normal text-[0.875rem] leading-[1.6rem]">Home v.3</a>
+            <a className="link link-hover font-normal text-[0.875rem] leading-[1.6rem]">Contact</a>
+            <a className="link link-hover font-normal text-[0.875rem] leading-[1.6rem]">Privacy Policy</a>
           </div>
           <div>
-            <span className="text-[0.85rem] text-white font-medium laptop:mb-4">/OUR WORKS</span>
-            <a className="link link-hover font-normal leading-[1.6rem]">Style Guide</a>
-            <a className="link link-hover font-normal leading-[1.6rem]">Instructions</a>
-            <a className="link link-hover font-normal leading-[1.6rem]">Licenses</a>
-            <a className="link link-hover font-normal">Changelog</a>
+            <span className="text-[0.9rem] text-white font-medium laptop:mb-4">/OUR WORKS</span>
+            <a className="link link-hover font-normal text-[0.875rem] leading-[1.6rem]">Style Guide</a>
+            <a className="link link-hover font-normal text-[0.875rem] leading-[1.6rem]">Instructions</a>
+            <a className="link link-hover font-normal text-[0.875rem] leading-[1.6rem]">Licenses</a>
+            <a className="link link-hover font-normal text-[0.875rem]">Changelog</a>
           </div>
           <div>
-            <span className="text-[0.85rem] text-white font-medium laptop:mb-4">/SOCIALS</span>
-            <a className="link link-hover font-normal leading-[1.6rem]">Style Guide</a>
-            <a className="link link-hover font-normal leading-[1.6rem]">Instructions</a>
-            <a className="link link-hover font-normal leading-[1.6rem]">Licenses</a>
-            <a className="link link-hover font-normal">Changelog</a>
+            <span className="text-[0.9rem] text-white font-medium laptop:mb-4">/SOCIALS</span>
+            <a className="link link-hover font-normal text-[0.875rem] leading-[1.6rem]">Style Guide</a>
+            <a className="link link-hover font-normal text-[0.875rem] leading-[1.6rem]">Instructions</a>
+            <a className="link link-hover font-normal text-[0.875rem] leading-[1.6rem]">Licenses</a>
+            <a className="link link-hover font-normal text-[0.875rem]">Changelog</a>
           </div>
         </div>
 
@@ -53,7 +53,7 @@ const Footer = () => {
 
       <footer className="footer mx-2 laptop:p-0 laptop:py-16 p-6 max-w-[90%] laptop:mx-[5.6rem] border border-white/25 border-x-transparent">
 
-        <div className="">
+        <div className="w-full">
           <img src={KakaoLogo} className="w-full" alt="web-logo" />
         </div>
 
@@ -70,7 +70,7 @@ const Footer = () => {
               className="py-3 px-0 bg-black text-white/75 min-w-[rem] input rounded-none "
             />
 
-            <button className="btn hover:bg-transparent hover:translate-x-4 absolute -top-0 right-4 rounded-none bg-transparent text-white border-none">
+            <button className="btn hover:bg-transparen px-2 py-0 bg-white text-black hover:translate-x-4 absolute top-10 right-4 rounded-full border-none">
               <FiArrowRight size="1.5rem" />
             </button>
           </div>
@@ -83,15 +83,15 @@ const Footer = () => {
 
       <div className="laptop:flex py-10 justify-between laptop:mx-[5.7rem]">
         {/* copyright */}
-        <p className="py-2 copyright laptop:mr-[rem] laptop:ml-0 mx-8 text-[1rem] laptop:block tablet:block text-[#ffffff74]">
+        <p className="py-2 copyright laptop:mr-[rem] laptop:ml-0 mx-8 text-[0.9rem] laptop:block tablet:block text-[#ffffff74]">
           @
           Kakao.
           All rights reserved
-          <span className="text-[0.85rem]"> {theDate}</span>.
+          <span className="text-[0.9rem]"> {theDate}</span>.
           Licensing
         </p>
 
-        <span className="py-2 copyright laptop:mr-8 mx-7 text-[1rem] laptop:block tablet:block text-white/50">
+        <span className="py-2 copyright laptop:mr-8 mx-7 text-[0.9rem] laptop:block tablet:block text-white/50">
           <a className="link link-hover text-white hover:text-white/50">
             Template
           </a>
