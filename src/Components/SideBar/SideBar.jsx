870775cd1504770f2758cf6cf845a57aaf865491
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-18 17:11:51 +0100

    Update SideBar.jsx

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -29,7 +29,7 @@ function MultiLevelSidebar() {
     };
 
     return (
-        <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 mr-0 block absolut bg-black text-white shadow-xl shadow-blue-gray-900/5">
+        <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 mr-0 block absolute bg-black text-white shadow-xl shadow-blue-gray-900/5">
             <div className="mb-2 p-4">
                 <Typography variant="h5" color="blue-gray">
                     Sidebar
